1c075cac31c1089efc7a889e5710571a48c58fcf
Guy Sion <42602229+guysion@users.noreply.github.com>
2019-02-14 09:32:52 +0100

    guycode.js added

---
+++ scripts/guycode.js
@@ -0,0 +1,57 @@
+var currentNoteIndex = 0;
+document.querySelector("#note").addEventListener('click', function() {
+    var note = abcsong.notes[currentNoteIndex];
+    var noteString = currentNoteIndex + " – Name: " + note.name + ", midi: " + note.midi + ", duration: " + note.duration;
+    console.log(noteString);
+    currentNoteIndex = (currentNoteIndex + 1) % abcsong.notes.length;
+});
+ var abcsong = {
+  "startTime": 0,
+  "duration": 24,
+  "length": 42,
+  "notes": [
+    {
+      "name": "C5",
+      "midi": 72,
+      "time": 0,
+      "velocity": 0.7952755905511811,
+      "duration": 0.5
+    },
+    {
+      "name": "C5",
+      "midi": 72,
+      "time": 0.5,
+      "velocity": 0.7952755905511811,
+      "duration": 0.5
+    },
+    {
+      "name": "G5",
+      "midi": 79,
+      "time": 1,
+      "velocity": 0.7952755905511811,
+      "duration": 0.5
+    },
+    {
+      "name": "G5",
+      "midi": 79,
+      "time": 1.5,
+      "velocity": 0.7952755905511811,
+      "duration": 0.5
+    },
+    {
+      "name": "A5",
+      "midi": 81,
+      "time": 2,
+      "velocity": 0.7952755905511811,
+      "duration": 0.5
+    },
+    {
+      "name": "A5",
+      "midi": 81,
+      "time": 2.5,
+      "velocity": 0.7952755905511811,
+      "duration": 0.5 
+    }
+  ]
+};
+</script>
